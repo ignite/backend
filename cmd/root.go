7245a3eb058a537de537d0583124c4a0c62b534d
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-06-07 15:58:28 +0200

    refactor: move Viper initialization to a function

--- cmd/root.go
+++ cmd/root.go
@@ -15,30 +15,34 @@ func New() *cobra.Command {
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			// Prepare Viper to read values from the environment
-			viper.SetEnvPrefix("BCB")
-			viper.AutomaticEnv()
-
-			// Initialize the flags with the values setted in the environment.
-			// This is done to be able to mark flags as required allowing their
-			// initialization to be done thought the environment.
-			cmd.Flags().VisitAll(func(f *pflag.Flag) {
-				if f.Name == "help" {
-					return
-				}
-
-				// Viper environment variable name for the flag
-				name := strings.ReplaceAll(f.Name, "-", "_")
-
-				// Assign the env value to the flag
-				if viper.IsSet(name) && viper.GetString(name) != "" {
-					cmd.Flags().Set(f.Name, viper.GetString(name))
-				}
-			})
+			initViper(cmd)
 		},
 	}
 
 	c.AddCommand(NewCollector())
 
 	return c
+}
+
+func initViper(cmd *cobra.Command) {
+	// Prepare Viper to read values from the environment
+	viper.SetEnvPrefix("BCB")
+	viper.AutomaticEnv()
+
+	// Initialize the flags with the values setted in the environment.
+	// This is done to be able to mark flags as required allowing their
+	// initialization to be done thought the environment.
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		if f.Name == "help" {
+			return
+		}
+
+		// Viper environment variable name for the flag
+		name := strings.ReplaceAll(f.Name, "-", "_")
+
+		// Assign the env value to the flag
+		if viper.IsSet(name) && viper.GetString(name) != "" {
+			cmd.Flags().Set(f.Name, viper.GetString(name))
+		}
+	})
 }
